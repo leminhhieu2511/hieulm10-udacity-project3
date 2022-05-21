bdc4581be4e83cd112d8ac75c275c6573f5b328b
viethai-git <75465111+viethai-git@users.noreply.github.com>
2022-05-21 16:06:12 +0700

    update docker

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+export POSTGRES_USERNAME=project3demo;
+export POSTGRES_PASSWORD=project3demo;
+export POSTGRES_HOST=project3demo.ceb8jel5yy24.us-east-1.rds.amazonaws.com;
+export POSTGRES_DB=project3demo;
+export AWS_BUCKET=project-3-demo;
+export AWS_REGION=us-east-1;
+export AWS_PROFILE=default;
+export JWT_SECRET=hello;
+export URL=http://localhost:8100;
---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  username: "project3demo",
+  password: "project3demo",
+  database: "project3demo",
+  host: "project3demo.ceb8jel5yy24.us-east-1.rds.amazonaws.com",
+  dialect: "postgres",
+  aws_region: "us-east-1",
+  aws_profile: "default",
+  aws_media_bucket: "project-3-demo",
+  url: "http://localhost:8100",
+  jwt: {
+    secret: "hello",
+  },
+};

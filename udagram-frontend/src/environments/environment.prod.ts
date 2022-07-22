1444a18f1c1b083b6fffab4682cceeeb985bae65
viethai-git <75465111+viethai-git@users.noreply.github.com>
2022-07-22 15:47:45 +0700

    update apiHost

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "a8a54bf9380944fd6a4d9a6694bf4027-1137846260.us-east-1.elb.amazonaws.com",
 };
 
 /*

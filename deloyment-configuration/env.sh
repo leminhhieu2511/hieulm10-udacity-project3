0669c0bee0bdd71ea9d012007deb2b75f1935947
viethai-git <75465111+viethai-git@users.noreply.github.com>
2022-07-22 15:18:31 +0700

    update yaml build

---
+++ deloyment-configuration/env.sh
@@ -0,0 +1,12 @@
+export POSTGRES_USERNAME=dataphatprod
+export POSTGRES_PASSWORD=dataphatprod
+export POSTGRES_HOST=dataphatprod.chll3lpk1rey.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=dataphatprod
+export AWS_BUCKET=arn:aws:s3:::mys3-udacity
+export AWS_REGION=us-east-1a
+export AWS_PROFILE=phatkt
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+export DOCKER_USERNAME=phatkhatuan
+export DOCKER_PASSWORD=2c3v@2023
+

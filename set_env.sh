4037171746c83cf2eddafa74e10727ffb499563d
Baoth1 <baoth1.work@gmail.com>
2022-12-28 23:31:17 +0700

    asdasda

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=project3demo;
-export POSTGRES_PASSWORD=project3demo;
-export POSTGRES_HOST=project3demo.ceb8jel5yy24.us-east-1.rds.amazonaws.com;
-export POSTGRES_DB=project3demo;
-export AWS_BUCKET=project-3-demo;
-export AWS_REGION=us-east-1;
-export AWS_PROFILE=default;
-export JWT_SECRET=hello;
-export URL=http://localhost:8100;
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=fpt123321
+export POSTGRES_HOST=udacityproject3baoth1.csyfrkxhnsrx.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project3bucket
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  username: "project3demo",
-  password: "project3demo",
-  database: "project3demo",
-  host: "project3demo.ceb8jel5yy24.us-east-1.rds.amazonaws.com",
-  dialect: "postgres",
-  aws_region: "us-east-1",
-  aws_profile: "default",
-  aws_media_bucket: "project-3-demo",
-  url: "http://localhost:8100",
-  jwt: {
-    secret: "hello",
+  'username': 'postgres',
+  'password': 'fpt123321',
+  'database': 'postgres',
+  'host': 'udacityproject3baoth1.csyfrkxhnsrx.ap-southeast-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'ap-southeast-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'project3bucket',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
   },
 };

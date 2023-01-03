369410aa5781e43774b096590d340eb834b3f2ab
Hiếu Lê <leminhhieu2511@gmail.com>
2023-01-03 19:45:30 +0700

    update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=hieulm10;
-export POSTGRES_PASSWORD=adminHieu;
-export POSTGRES_HOST=hieulm10-rds.czcehnlvbzi7.us-west-1.rds.amazonaws.com;
-export POSTGRES_DB=postgres;
-export AWS_BUCKET=udacity-aws-project3-s3;
-export AWS_REGION=us-west-1;
-export AWS_PROFILE=default;
-export JWT_SECRET=testing;
-export URL=http://localhost:8100;
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=adminHieu
+export POSTGRES_HOST=project3-db-instance.csyfrkxhnsrx.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacity-aws-project3-s3
+export AWS_REGION=us-west-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100

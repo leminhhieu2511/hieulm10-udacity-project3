ed2b3f3005419e73241664df7788609008d749ab
Hiếu Lê <leminhhieu2511@gmail.com>
2023-01-03 19:54:30 +0700

    update config

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
-  'username': 'hieulm10',
+  'username': 'postgres',
   'password': 'adminHieu',
   'database': 'postgres',
-  'host': 'hieulm10-rds.czcehnlvbzi7.us-west-1.rds.amazonaws.com',
+  'host': 'project3-db-instance.csyfrkxhnsrx.ap-southeast-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': 'us-west-1',
+  'aws_region': 'ap-southeast-1',
   'aws_profile': 'default',
-  'aws_media_bucket': 'udacity-aws-project3-s3',
+  'aws_media_bucket': 'project3bucket',
   'url': 'http://localhost:8100',
   'jwt': {
     'secret': 'testing',
